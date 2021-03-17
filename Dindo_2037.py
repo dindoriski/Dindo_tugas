be950f4b26850b13ffd8f234159a123dc2804f67
dindoriski <80688298+dindoriski@users.noreply.github.com>
2021-03-17 20:29:55 +0700

    Add files via upload

---
+++ Dindo_2037.py
@@ -0,0 +1,11 @@
+print('    SELAMAT DATANG     ')
+print('=======================')
+
+x=int(input("Masukkan bilangan pertama : "))
+y=int(input("Masukkan bilangan kedua : "))
+if x==y:
+	print("Bilangan pertama dan kedua sama besar")
+elif x>y:
+	print("Bilangan pertama lebih besar dari bilangan kedua")
+else:
+	print("Bilangan kedua lebih besar dari bilangan pertama")
